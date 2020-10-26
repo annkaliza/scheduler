a5ff3d46328f883fa5beadd65a6059ac3c62adf8
Kaliza Anna <annkaliza@gmail.com>
2020-10-26 21:59:45 +0000

    added 3 components ie Button.js, DayList.JS and DayListItem.js. Also added some code in Application.js

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,7 +2,7 @@ import React from "react";
 import DayListItem from "components/DayListItem";
 
 export default function DayList(props) {
-  const DayListData = props.days.map((day) => (
+  const DayListData = props.days.map((day) => ( 
     <DayListItem
       key={day.id}
       name={day.name}
