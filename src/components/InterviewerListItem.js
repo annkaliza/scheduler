efb12da58e6d8b3528aac733c092e07a8a45f8f3
Kaliza Anna <annkaliza@gmail.com>
2020-10-27 13:17:01 +0000

    added interviewlist item implementation

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,15 +1,19 @@
 import React from "react";
 import "components/InterviewerListItem.scss";
+import classnames from "classnames";
 
 export default function InterviewerListItem(props) {
+  const interviewerClass = classnames({"interviews__item": true, "interviews__item--selected": props.selected});
+  const imageClass = classnames({"interviews__item-image": true, "interviews__item--selected-image": props.selected});
+
   return (
-    <li className="interviewers__item">
+    <li className={interviewerClass} onclick={props.setInterviewer}>
       <img
-        className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        className={imageClass}
+        src={props.avatar}
+        alt={props.name}
       />
-      Sylvia Palmer
+      {props.selected && props.name}
     </li>
   );
 }
