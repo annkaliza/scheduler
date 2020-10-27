4b37e9aa27c532f52e27ee877285482850a44447
Kaliza Anna <annkaliza@gmail.com>
2020-10-27 13:51:53 +0000

    added iinterviewer list implementations

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,13 +1,16 @@
 import React from "react";
-
+import InterviewerListItem from "components/InterviewerListItem";
 
 export default function InterviewerList(props) {
 
+  const interviewerArray = props.interviewers.map((interviewer) =>{
+    <InterviewerListItem key ={interviewer.id} name = {interviewer.name} selected = {interviewer.id === props.value} setInterviewer = {(e) +> props.onChange(interviewer.id)}/>
 
+  });
   return (
     <section className="interviewers">
     <h4 className="interviewers__header text--light">Interviewer</h4>
-    <ul className="interviewers__list"></ul>
+  <ul className="interviewers__list">{interviewerArray}</ul>
   </section>
   );
 }
