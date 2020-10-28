e649277dfc1f781ffc4aff80d7310a8fad139716
Anna Kaliza <annkaliza@gmail.com>
2020-10-28 09:16:10 -0400

    added show component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "components/Appointment/styles.scss";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/show";
 
 export default function Appointment(props){
   return(
