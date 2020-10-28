aca7e26bea7c776b7d753817d3c2fd164fdb3593
Anna Kaliza <annkaliza@gmail.com>
2020-10-28 12:13:09 -0400

    added confirm and status components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,6 +3,8 @@ import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
 
 export default function Appointment(props){
   return(
